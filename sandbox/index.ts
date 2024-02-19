b98dc9314cae972793165bd923938835d713990a
auza0002 <auzajuan1@gmail.com>
2024-02-19 10:36:48 -0500

    hashes - null values

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -2,11 +2,14 @@ import 'dotenv/config';
 import { client } from '../src/services/redis';
 
 const run = async () => {
-    await client.hSet('item',{
-        color: 'red',
-        year: 1950
-    })
-    const item = await client.hGetAll('item');
+    // await client.hSet('item', {
+	// 		color: 'red',
+	// 		year: 1950,
+	// 		engines: { cylinders: 8 },
+	// 		owner: 'null',
+	// 		service: 'undefined'
+	// 	});
+    const item = await client.hGetAll('cars#553');
     console.log(item)
 };
 run();
