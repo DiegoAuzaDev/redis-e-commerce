409d59976818909aaf95b0f28c84c4bda8f1d426
auza0002 <auzajuan1@gmail.com>
2024-02-19 10:23:16 -0500

    HSET

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -1,5 +1,12 @@
 import 'dotenv/config';
 import { client } from '../src/services/redis';
 
-const run = async () => {};
+const run = async () => {
+    await client.hSet('item',{
+        color: 'red',
+        year: 1950
+    })
+    const item = await client.hGetAll('item');
+    console.log(item)
+};
 run();
