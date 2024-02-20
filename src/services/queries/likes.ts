2033a5570c674933b3b1ee123a011f7a44111d64
auza0002 <auzajuan1@gmail.com>
2024-02-20 09:36:41 -0500

    unlike and like items

--- src/services/queries/likes.ts
+++ src/services/queries/likes.ts
@@ -1,9 +1,24 @@
-export const userLikesItem = async (itemId: string, userId: string) => {};
+import { itemKey, userLikesKey } from '$services/keys';
+import { client } from '$services/redis';
+
+export const userLikesItem = async (itemId: string, userId: string) => {
+	return await client.sIsMember(userLikesKey(userId), itemId);
+};
 
 export const likedItems = async (userId: string) => {};
 
-export const likeItem = async (itemId: string, userId: string) => {};
+export const likeItem = async (itemId: string, userId: string) => {
+	const inserted = await client.sAdd(userLikesKey(userId), itemId);
+	if (inserted) {
+		return await client.hIncrBy(itemKey(itemId), 'likes', 1);
+	}
+};
 
-export const unlikeItem = async (itemId: string, userId: string) => {};
+export const unlikeItem = async (itemId: string, userId: string) => {
+	const removed = await client.sRem(userLikesKey(userId), itemId);
+	if (removed) {
+		return await client.hIncrBy(itemKey(itemId), 'likes', -1);
+	}
+};
 
 export const commonLikedItems = async (userOneId: string, userTwoId: string) => {};
