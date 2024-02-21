335fdb54924c189b57ead0db4c47850971431731
auza0002 <auzajuan1@gmail.com>
2024-02-21 13:10:39 -0500

    Sorted Set ZADD, ZRANGE

--- src/services/queries/likes.ts
+++ src/services/queries/likes.ts
@@ -1,11 +1,15 @@
 import { itemKey, userLikesKey } from '$services/keys';
 import { client } from '$services/redis';
+import { getItems } from './items';
 
 export const userLikesItem = async (itemId: string, userId: string) => {
 	return await client.sIsMember(userLikesKey(userId), itemId);
 };
 
-export const likedItems = async (userId: string) => {};
+export const likedItems = async (userId: string) => {
+	const ids = await client.sMembers(userLikesKey(userId));
+	return getItems(ids);
+};
 
 export const likeItem = async (itemId: string, userId: string) => {
 	const inserted = await client.sAdd(userLikesKey(userId), itemId);
@@ -21,4 +25,7 @@ export const unlikeItem = async (itemId: string, userId: string) => {
 	}
 };
 
-export const commonLikedItems = async (userOneId: string, userTwoId: string) => {};
+export const commonLikedItems = async (userOneId: string, userTwoId: string) => {
+	const ids = await client.sInter([userLikesKey(userOneId), userLikesKey(userTwoId)]);
+	return getItems(ids);
+};
