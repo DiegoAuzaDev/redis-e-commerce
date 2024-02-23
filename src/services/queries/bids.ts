ffe1dd41a7a76c1607b7d333bf8d5cd695705e6c
auza0002 <auzajuan1@gmail.com>
2024-02-23 11:58:46 -0500

    Working with List and Bids

--- src/services/queries/bids.ts
+++ src/services/queries/bids.ts
@@ -1,6 +1,8 @@
 import type { CreateBidAttrs, Bid } from '$services/types';
 
-export const createBid = async (attrs: CreateBidAttrs) => {};
+export const createBid = async (attrs: CreateBidAttrs) => {
+	
+};
 
 export const getBidHistory = async (itemId: string, offset = 0, count = 10): Promise<Bid[]> => {
 	return [];
