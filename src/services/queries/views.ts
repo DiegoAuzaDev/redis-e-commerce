62a9eb65fd0099c72633c7233e49039fa0a78ef6
auza0002 <auzajuan1@gmail.com>
2024-03-04 11:14:58 -0500

    Adding Lua Scripts Node-Redis

--- src/services/queries/views.ts
+++ src/services/queries/views.ts
@@ -3,6 +3,7 @@ import { client } from '$services/redis';
 
 export const incrementView = async (itemId: string, userId: string) => {
 	const inserted = await client.pfAdd(itemsViewkey(itemId), userId);
+	
 	if (inserted) {
 		return Promise.all([
 			client.hIncrBy(itemKey(itemId), 'views', 1),
