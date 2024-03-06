8ce1fb1fd4acb999c9e4da661f6e672015ce25a0
auza0002 <auzajuan1@gmail.com>
2024-03-06 12:55:59 -0500

    Setting up Lock

--- src/services/redis/lock.ts
+++ src/services/redis/lock.ts
@@ -1,4 +1,10 @@
-export const withLock = async () => {};
+export const withLock = async (key: string, cb  : ()=> any) => {
+	const retryDelaysMs = 100;
+	let retries = 20;
+
+	// Generate a random value to store at the look key
+};
+
 
 const buildClientProxy = () => {};
 
